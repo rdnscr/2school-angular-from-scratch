6166f2b6b0a41d948586b07160438bb2081a82a8
Raphael Ritter <raphael.ritter@2BIT.ch>
2022-05-01 10:52:34 +0200

    migrated to eslint

--- src/app/todo/add/todo-add.component.ts
+++ src/app/todo/add/todo-add.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { TodoItem } from '../todo.type';
-import { FormGroupDirective, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-todo-add',
   templateUrl: 'todo-add.component.html',
 })
-export class TodoAddComponent implements OnInit {
+export class TodoAddComponent {
   public newTodoForm = new FormGroup({
     description: new FormControl(null, [Validators.required]),
   });
 
   constructor(private todoService: TodoService) {}
-
-  ngOnInit() {}
 
   public onAdd(form: FormGroupDirective) {
     if (this.newTodoForm.valid && this.newTodoForm.dirty) {
