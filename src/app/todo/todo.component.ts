83c1139fdc7c0bc5a8ec51c7a5bae59bbdeec7c9
rdnscr <raphael.ritter@bluewin.ch>
2024-06-01 11:01:02 +0200

    #5 Check and uncheck Todos and display in separate lists

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,11 +3,14 @@ import { TodoItem } from './todo.type';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
+import { TodosPipe } from './todos.pipe';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   standalone: true,
-  imports: [MatCardModule, MatListModule, CommonModule],
+  imports: [MatCardModule, MatListModule, MatCheckboxModule, FormsModule, CommonModule, TodosPipe],
   styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent {
@@ -18,8 +21,8 @@ export class TodoComponent {
       description: 'Erste Angular Applikation erstellen',
     },
     { id: 2, checked: false, description: 'Todo Komponente erstellen' },
-    { id: 3, checked: false, description: 'Todos anzeigen' },
-    { id: 4, checked: false, description: 'Todos hinzufügen' },
+    { id: 3, checked: true, description: 'Todos anzeigen' },
+    { id: 4, checked: true, description: 'Todos hinzufügen' },
     { id: 5, checked: false, description: 'Todos erledigen' },
   ];
 }
