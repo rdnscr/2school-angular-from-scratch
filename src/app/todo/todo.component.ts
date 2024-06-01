58101634e6abcec4d31ff8e4aeb35216653788b8
rdnscr <raphael.ritter@bluewin.ch>
2024-06-01 10:48:09 +0200

    # display available todos in a list

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo.type';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   standalone: true,
+  imports: [MatCardModule, MatListModule, CommonModule],
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {}
+export class TodoComponent {
+  public items: Array<TodoItem> = [
+    {
+      id: 1,
+      checked: false,
+      description: 'Erste Angular Applikation erstellen',
+    },
+    { id: 2, checked: false, description: 'Todo Komponente erstellen' },
+    { id: 3, checked: false, description: 'Todos anzeigen' },
+    { id: 4, checked: false, description: 'Todos hinzufügen' },
+    { id: 5, checked: false, description: 'Todos erledigen' },
+  ];
+}
