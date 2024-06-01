8c9926a4196f8f566d5da92b63df7c2ee58a36cf
rdnscr <raphael.ritter@bluewin.ch>
2024-06-01 10:33:54 +0200

    #3 add todo component and display it

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-navigation',
@@ -21,6 +22,7 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
+    TodoComponent
   ]
 })
 export class NavigationComponent {
